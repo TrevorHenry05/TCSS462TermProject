dc61c4f8ee690e6e81f13afc448e79a5bc7abd21
TrevorHenry05 <trevorjhen03@gmail.com>
2023-12-10 17:03:12 -0800

    Changed the lambda handlers to get key and bucket name from request instead of s3 event.

--- transform_function.py
+++ transform_function.py
@@ -1,4 +1,6 @@
 import boto3
+import json
+import logging
 
 from operations import transform
 
@@ -6,10 +8,17 @@ from operations import transform
 s3_client = boto3.client('s3')
 transformed_csv_bucket_name = "service2csvbucket"
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
     # Get bucket name and file key from the S3 event
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    file_key = event['Records'][0]['s3']['object']['key']
+    # bucket_name = event['Records'][0]['s3']['bucket']['name']
+    # file_key = event['Records'][0]['s3']['object']['key']
+    logger.info(event)
+    body = json.loads(event["body"])
+    bucket_name = body["bucket_name"]
+    file_key = body["key"]
 
     # Download the file from S3
     s3_client.download_file(bucket_name, file_key, '/tmp/' + file_key)
@@ -17,5 +26,17 @@ def lambda_handler(event, context):
     transformed_file_path = transform('/tmp/' + file_key)
 
     # Read the transformed data and upload to S3
+    key = 'transformed_' + file_key
     with open(transformed_file_path, 'rb') as f:
-        s3_client.put_object(Body=f, Bucket=transformed_csv_bucket_name, Key='transformed_' + file_key)
+        s3_client.put_object(Body=f, Bucket=transformed_csv_bucket_name, Key=key)
+        
+    return {
+        'statusCode': 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        'body': json.dumps({
+            'bucket_name': transformed_csv_bucket_name,
+            'key': key
+        })
+    }
